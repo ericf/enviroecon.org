443b26783156d932b57114eec7edb69c3a30d471
Eric Ferraiuolo <eferraiuolo@gmail.com>
2014-03-02 14:20:27 -0500

    Improve styling of venue info overlay

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@
         }
     });
 
-    map.setView([42.364, -71.075], screen.width >= 480 ? 14 : 13);
+    map.setView([42.366, -71.075], screen.width >= 480 ? 14 : 13);
     map.scrollWheelZoom.disable();
     map.zoomControl.setPosition('bottomleft');
 }(this));
